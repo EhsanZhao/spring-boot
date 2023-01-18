cb8d3b8ab703cf0b63a28fe215dee8aa25867cc0
zhaoyuan <me@ehsanzhao.com>
2023-01-18 15:20:16 +0800

    spring boot

---
+++ src/main/java/com/ehsanzhao/springboot/MainApplication.java
@@ -0,0 +1,17 @@
+package com.ehsanzhao.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhaoyuan
+ * @date 2023/1/18
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class);
+    }
+
+}
---
+++ src/main/java/com/ehsanzhao/springboot/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.ehsanzhao.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhaoyuan
+ * @date 2023/1/18
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+       return "Hi,Spring boot";
+    }
+
+}
