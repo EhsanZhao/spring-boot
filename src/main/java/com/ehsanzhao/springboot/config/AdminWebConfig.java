f9a7789c3cd5dbf5470e2407314faa723d3cb8d8
zhaoyuan <me@ehsanzhao.com>
2023-02-16 15:02:21 +0800

    定制化web功能

--- src/main/java/com/ehsanzhao/springboot/config/AdminWebConfig.java
+++ src/main/java/com/ehsanzhao/springboot/config/AdminWebConfig.java
@@ -1,24 +1,52 @@
 package com.ehsanzhao.springboot.config;
 
 import com.ehsanzhao.springboot.intercepter.LoginIntercepter;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 /**
  * @author zhaoyuan
  * @date 2023/2/14
+ * @EnableWebMvc（慎用）  全面接管mvc  1、静态资源？试图解析器？欢迎……页全部失效
+ * 所有的规则全部要自己重新配置
  */
+//@EnableWebMvc
 @Configuration
 public class AdminWebConfig implements WebMvcConfigurer {
+
+    /**
+     * 访问 /aa/** 都有请求都去 classpath:/static/ 下面进行匹配
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/aa/**")
+                .addResourceLocations("classpath:/static/");
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginIntercepter())
                 //所有资源都会拦截，静态资源也会被拦截
                 .addPathPatterns("/**")
-                .excludePathPatterns("/","/login","/resources/**","/hello","/error");
+                .excludePathPatterns("/","/login","/resources/**","/hello","/error","/aa/**");
     }
+
+//    @Bean
+//    public WebMvcRegistrations webMvcRegistrations(){
+//        return new WebMvcRegistrations() {
+//            @Override
+//            public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
+//                return WebMvcRegistrations.super.getRequestMappingHandlerMapping();
+//            }
+//        };
+//    }
 
 
 }
