8e03a70514169123256c60c1fd5d426c04fe59ed
zhaoyuan <me@ehsanzhao.com>
2023-02-27 15:00:30 +0800

    mybatis starter方式整合 insert

--- src/main/java/com/ehsanzhao/springboot/mapper/UserMapper.java
+++ src/main/java/com/ehsanzhao/springboot/mapper/UserMapper.java
@@ -1,7 +1,9 @@
 package com.ehsanzhao.springboot.mapper;
 
 import com.ehsanzhao.springboot.entity.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -17,5 +19,11 @@ public interface UserMapper {
 
     @Select("select * from user where host = #{host}")
     List<User> getUserByHost(String host);
+
+    @Insert("insert into xxx (xxx)values(#{host}) ")
+    // useGeneratedKeys这会令 MyBatis 使用 JDBC 的 getGeneratedKeys 方法来取出由数据库内部生成的主键
+    // keyProperty指定能够唯一识别对象的属性，MyBatis 会使用 getGeneratedKeys 的返回值或 insert 语句的 selectKey 子元素设置它的值
+    @Options(useGeneratedKeys = true,keyProperty = "id")
+    List<User> insert(String host);
 
 }
